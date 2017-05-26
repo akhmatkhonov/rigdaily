cdbc4f92c957c424d00103cbea3b1e608ed6b43c
KupchinskiyVasiliy <vasiliy.kupchinskiy@gmail.com>
2017-05-26 12:50:37 +0700

    Added packages for live development

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     cleanCss = require('gulp-clean-css'),
-    htmlmin = require('gulp-htmlmin');
+    htmlmin = require('gulp-htmlmin'),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect');
 
 gulp.task('js', function () {
     gulp.src(['app/js/apiclient/*.js'])
@@ -11,7 +13,8 @@ gulp.task('js', function () {
         .pipe(concat('js/rigdaily.js'))
         .pipe(gulp.dest('dist'));
     gulp.src(['app/js/jquery-*.js'])
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(connect.reload());
 });
 
 gulp.task('css', function () {
@@ -19,19 +22,34 @@ gulp.task('css', function () {
         .pipe(cleanCss({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist/css'));
     gulp.src(['app/css/jquery-*.css'])
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('images', function () {
     gulp.src(['app/css/images/*'])
-        .pipe(gulp.dest('dist/css/images'));
+        .pipe(gulp.dest('dist/css/images'))
+        .pipe(connect.reload());
 });
 
-gulp.task('page', function () {
+gulp.task('html', function () {
     gulp.src(['app/*.htm'])
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload());
 });
 
-gulp.task('default', ['js', 'css', 'images', 'page'], function () {
+gulp.task('connect', function () {
+    connect.server({
+        root: 'dist',
+        livereload: true
+    });
 });
+
+gulp.task('watch', function () {
+    gulp.watch('./app/css/**/*', ['css']);
+    gulp.watch('./app/js/**/*', ['js']);
+    gulp.watch('./app/*.html', ['html']);
+});
+
+gulp.task('default', ['js', 'css', 'images', 'html', 'connect', 'watch']);
